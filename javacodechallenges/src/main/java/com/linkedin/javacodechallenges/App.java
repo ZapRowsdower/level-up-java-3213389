2de14053c3ea08cb754242725890835aaf414a6d
ZapRowsdower <ZapRowsdower@users.noreply.github.com>
2023-10-30 17:33:59 +0000

    my waterbill impl

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -3,10 +3,22 @@ package com.linkedin.javacodechallenges;
 import java.util.Scanner;
 
 public class App {
+    private static int one_ccf_in_gallons = 748;
+    private static double dollar_per_ccf = 9.42;
+    private static double min_charge = 18.84;
+    private static double overcharge_threshold = 2;
+    private static double overcharge_amt = 3.90;
 
     public static double calculateWaterBill(double gallonsUsage) {
-        // TODO: Implement method
-        return 0;
+        var convert_gallons_to_ccf = gallonsUsage / one_ccf_in_gallons;
+        var whole_ccf = Math.ceil(convert_gallons_to_ccf);
+        var result = 0.0;
+        if ((convert_gallons_to_ccf) <= 2) {
+            result = min_charge;
+        } else if ((convert_gallons_to_ccf > 2)) {
+            result = min_charge + (overcharge_amt * (whole_ccf - overcharge_threshold));
+        }
+        return result;
     }
 
     public static void main(String[] args) {
