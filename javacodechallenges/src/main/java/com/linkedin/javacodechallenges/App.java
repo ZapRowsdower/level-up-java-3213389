2b593583c29be120bff21eae72a66a8595c11ab7
ZapRowsdower <ZapRowsdower@users.noreply.github.com>
2023-10-30 19:11:09 +0000

    naive 100 days date impl

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,13 +1,18 @@
 package com.linkedin.javacodechallenges;
 
-public class App 
-{
-    // Create function to calculate the date that's 
+import java.util.Date;
+
+public class App {
+    // Create function to calculate the date that's
     // 100 days from now
 
-    public static void main( String[] args )
-    {
-        System.out.println("100 days from now is... " 
-            /* add function call */);
+    public static void main(String[] args) {
+        System.out.println("100 days from now is... " + oneHundredDays(new Date()));
+    }
+
+    public static String oneHundredDays(Date today) {
+        var ms_in_day = 8.64e7;
+        long one_hundred_in_future = today.getTime() + (long) (ms_in_day * 100);
+        return new Date(one_hundred_in_future).toString();
     }
 }
